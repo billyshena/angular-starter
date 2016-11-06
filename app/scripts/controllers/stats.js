7addea52080d81582c31b8f209b3e9532f8874cd
Billy Shen <billy.shen@hetic.net>
2016-11-06 11:24:18 +0100

    [ADD] Kalinox graph

--- app/scripts/controllers/stats.js
+++ app/scripts/controllers/stats.js
@@ -47,5 +47,58 @@ angular.module('airliquideApp')
           { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }
         ]
       }
-    ]
+    ];
+
+
+
+    Highcharts.chart('container', {
+
+     chart: {
+         type: 'column'
+     },
+
+     title: {
+         text: 'Consommation totale Kalinox'
+     },
+
+     xAxis: {
+         categories: ['Hommes', 'Femmes', 'Enfants']
+     },
+
+     yAxis: {
+         allowDecimals: false,
+         min: 0,
+         max: 100,
+         tickInterval: 100,
+         title: {
+             text: 'Pourcentage patient'
+         }
+     },
+
+     tooltip: {
+         formatter: function () {
+             return '<b>' + this.x + '</b><br/>' +
+                 this.series.name + ': ' + this.y + '<br/>';
+         }
+     },
+
+     plotOptions: {
+         column: {
+             stacking: 'normal'
+         }
+     },
+     credits: {
+       enabled: false
+     },
+
+     series: [{
+         name: 'Total',
+         data: [20, 30, 50]
+     },
+     {
+       name: 'Kalinox',
+       data: [3, 10, 40]
+     }
+   ]
+ });
 });
